51187e4736a21ef5a5740fff2f737399a29515b3
William Marroush <William.Marroush@csi.cuny.edu>
2024-12-17 15:55:40 -0500

    fixing backend port

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 const authRoutes = require('./routes/authRoutes');
 
